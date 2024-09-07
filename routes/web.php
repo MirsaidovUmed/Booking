f576660775f265fae8da8c7dbbb7112c672ff2b5
MirsaidovUmed <umedmirsaidov61@gmail.com>
2024-09-07 20:33:27 +0500

    changed routing for admins and users

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoomController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -32,10 +33,17 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+Route::group(['middleware' => ['auth', 'admin']], function() {
+    Route::resource('/hotels', HotelController::class);
+    Route::resource('/rooms', RoomController::class);
+    Route::resource('/facilities', RoomController::class);
+});
+
 Route::get('/hotels', [HotelController::class, 'index'])->name('hotels.index');
-Route::post('/hotels', [HotelController::class, 'store'])->name('hotels.store');
-Route::put('/hotels/{id}', [HotelController::class, 'update'])->name('hotels.update');
-Route::delete('/hotels/{id}', [HotelController::class, 'destroy'])->name('hotels.delete');
+Route::get('/hotels/{id}', [HotelController::class, 'getHotelById'])->name('hotels.show');
+// Route::post('/hotels', [HotelController::class, 'store'])->name('hotels.store');
+// Route::put('/hotels/{id}', [HotelController::class, 'update'])->name('hotels.update');
+// Route::delete('/hotels/{id}', [HotelController::class, 'destroy'])->name('hotels.delete');
 
 Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.index');
 Route::post('/bookings', [BookingController::class, 'store'])->name('bookings.store');

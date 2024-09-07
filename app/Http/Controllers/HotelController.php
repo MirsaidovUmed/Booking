9e44d2f5b994987fe0efb6dd42e22de519ce54e9
MirsaidovUmed <umedmirsaidov61@gmail.com>
2024-09-07 16:46:36 +0500

    added service classes to improve code quality

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -3,54 +3,48 @@
 namespace App\Http\Controllers;
 
 use App\Models\Hotel;
+use App\Services\HotelService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class HotelController extends Controller
 {
+
+    public $hotelService;
+
+    public function __construct(HotelService $hotelService)
+    {
+        $this->hotelService = $hotelService;
+    }
+
     public function index(): View
     {
-        $hotels = Hotel::paginate(10);
+        $hotels = $this->hotelService->index();
         return view('hotels.index', compact('hotels'));
+    }
+
+    public function getHotelById(int $id): View
+    {
+        $hotel = $this->hotelService->getHotelById($id);
+        return view('hotels.show', compact('hotel'));
     }
     
     public function store(Request $request): RedirectResponse
     {
-        $hotel = new Hotel;
-        $request->validate([
-            "title" => "required|max:255",
-            "description" => "required|max:255",
-            "poster_url"=>"required|website",
-            "address"=> "required",
-        ]);
-        $hotel->create($request->all());
+        $this->hotelService->create($request);
         return back()->with('status', 'Hotel Added successfully');
     }
 
     public function update(Request $request, int $id): RedirectResponse
     {
-        $hotel = Hotel::findOrFail($id);
-        $request->validate([
-            "title" => "required|max:255",
-            "description" => "required|max:255",
-            "poster_url"=>"required|website",
-            "address"=> "required",
-        ]);
-
-        $hotel->title = $request->input('title') ?? $hotel->title;
-        $hotel->description = $request->input('description') ?? $hotel->description;
-        $hotel->poster_url = $request->input('poster_url') ?? $hotel->poster_url;
-        $hotel->address = $request->input('address') ?? $hotel->address;
-        
-        $hotel->update();
-        return back()->with('status', 'Hotel Updated successfully');
+        $this->hotelService->update($request, $id);
+        return back()->with('status', 'Hotel updated successfully');
     }
-
+    
     public function destroy(int $id): RedirectResponse
     {
-        $hotel = Hotel::findOrFail($id);
-        $hotel->delete();
+        $this->hotelService->delete($id);
         return back()->with('status', 'Hotel deleted successfully');
     }
 }

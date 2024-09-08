aaf09b0e814168cf89f57cde8c96642e3f7bd1db
MirsaidovUmed <umedmirsaidov61@gmail.com>
2024-09-08 11:52:19 +0500

    added register and login buttons to navigation blade

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,6 +24,18 @@
                 </div>
             </div>
 
+            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link href="{{ route('register') }}">
+                    {{ __('Register') }}
+                    </x-nav-link>
+            </div>
+
+            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link href="{{ route('login') }}">
+                    {{ __('Login') }}
+                    </x-nav-link>
+            </div>
+
             <!-- Settings Dropdown -->
             @if(auth()->check())
                 <div class="hidden sm:flex sm:items-center sm:ml-6">

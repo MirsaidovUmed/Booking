44f5805a7f9be5c5fb42a7f95bae61aab18b14b3
Умед Мирсаидов <umedmirsaidov61@gmail.com>
2024-09-27 22:11:23 +0500

    fixed booking test and events for booking

--- app/Listeners/SendBookingConfirmation.php
+++ app/Listeners/SendBookingConfirmation.php
@@ -4,8 +4,10 @@ namespace App\Listeners;
 
 use App\Events\BookingCreated;
 use App\Mail\BookingCompletedMailing;
+use App\Models\Booking;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class SendBookingConfirmation
@@ -13,16 +15,21 @@ class SendBookingConfirmation
     /**
      * Create the event listener.
      */
-    // public function __construct()
-    // {
-    //     //
-    // }
+
+    public $booking;
+    public $user;
+
+    public function __construct(Booking $booking)
+    {
+        $this->booking = $booking;
+        $this->user = $booking->user;
+    }
 
     /**
      * Handle the event.
      */
     public function handle(BookingCreated $event): void
     {
-        Mail::to($event->booking->user->email)->send(new BookingCompletedMailing($event->booking->user));
+        Mail::to($event->booking->user->email)->send(new BookingCompletedMailing($event->booking));
     }
 }

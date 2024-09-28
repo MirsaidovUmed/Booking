2394f87a66c8d174295e958ca2d127048206a6bc
Умед Мирсаидов <umedmirsaidov61@gmail.com>
2024-09-28 23:45:57 +0500

    changed blades for correct views

--- resources/views/hotels/show.blade.php
+++ resources/views/hotels/show.blade.php
@@ -5,24 +5,33 @@
 
 <x-app-layout>
     <div class="py-14 px-4 md:px-6 2xl:px-20 2xl:container 2xl:mx-auto">
-        <!-- Информация по отелю -->
         <div class="flex flex-wrap mb-12">
             <div class="w-full flex justify-start md:w-1/3 mb-8 md:mb-0">
                 <img class="h-full rounded-l-sm" src="{{ $hotel->poster_url }}" alt="Hotel Image">
             </div>
             <div class="w-full md:w-2/3 px-4">
-                <div class="text-2xl font-bold">{{ $hotel->title }}</div> <!-- Исправлено поле с name на title -->
+                <div class="text-2xl font-bold">{{ $hotel->title }}</div>
                 <div class="flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1 text-blue-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0-2c3.31 0 6 2.69 6 6s-2.69 6-6-6-6-2.69-6-6 2.69-6 6-6z" />
                     </svg>
-                    {{ $hotel->address }} <!-- Адрес отеля -->
+                    {{ $hotel->address }}
                 </div>
-                <div>{{ $hotel->description }}</div> <!-- Описание отеля -->
+                <div>{{ $hotel->description }}</div>
             </div>
         </div>
 
-        <!-- Форма бронирования -->
+        @if($startDate && $endDate)
+            <div class="mt-12">
+                <h2 class="text-2xl font-bold mb-4">Доступные комнаты</h2>
+                <div class="flex flex-col w-full lg:w-4/5">
+                    @foreach($rooms as $room)
+                        <x-rooms.room-list-item :room="$room" class="mb-4"/>
+                    @endforeach
+                </div>
+            </div>
+        @endif
+
         <div class="flex flex-col">
             <div class="text-2xl text-center md:text-start font-bold">Забронировать комнату</div>
             <form method="get" action="{{ url()->current() }}">
@@ -47,13 +56,12 @@
             </form>
         </div>
 
-        <!-- Список отзывов -->
         <div class="mt-12">
             <h2 class="text-2xl font-bold mb-4">Отзывы</h2>
             @if($hotel->reviews->isNotEmpty())
                 @foreach($hotel->reviews as $review)
                     <div class="border-b py-4">
-                        <div class="font-bold">{{ $review->user->name }}</div> <!-- Имя пользователя, оставившего отзыв -->
+                        <div class="font-bold">{{ $review->user->name }}</div>
                         <div class="text-sm text-gray-600">
                             @if($review->created_at)
                                 {{ $review->created_at->format('Y-m-d') }}
@@ -61,8 +69,8 @@
                                 Дата не указана
                             @endif
                         </div>
-                        <div class="mt-2 font-bold text-yellow-500">Отзыв: {{ $review->review}}</div> <!-- Отзыв -->
-                        <div class="mt-2 font-bold text-yellow-500">Рейтинг: {{ $review->rating }}/5</div> <!-- Рейтинг -->
+                        <div class="mt-2 font-bold text-yellow-500">Отзыв: {{ $review->review}}</div>
+                        <div class="mt-2 font-bold text-yellow-500">Рейтинг: {{ $review->rating }}/5</div>
                     </div>
                 @endforeach
             @else
